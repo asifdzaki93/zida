dc6850c57853fa05428ef57214958f7ce6fcf1c8
candrawardana <38995778+candrawardana@users.noreply.github.com>
2024-05-06 01:04:35 +0700

    calendar_finish

--- admin/page/home.php
+++ admin/page/home.php
@@ -553,10 +553,10 @@ $icon = getPercentageChangeIcon($percentageChange);
     }
 
     function getRandomColor() {
-        var varters = '0123456789ABCDEF';
+        var letters = '0123456789ABCDEF';
         var color = '#';
         for (var i = 0; i < 6; i++) {
-            color += varters[Math.floor(Math.random() * 16)];
+            color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
     }
