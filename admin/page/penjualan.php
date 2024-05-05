dd0f41ae2020a0c48a53b1c924d723bb12f1a9d1
asifdzaki93 <asifdzaki93@gmail.com>
2024-05-06 04:43:34 +0700

    t

--- admin/page/penjualan.php
+++ admin/page/penjualan.php
@@ -2,12 +2,11 @@
 include "data/koneksi.php";
 include "data/nota_admin.php";
 include "data/base_sistem.php";
-nota_admin_render($mysqli,$base_url);
+nota_admin_render($mysqli, $base_url);
 ?>
 <div class="card">
     <div class="card-datatable table-responsive pt-0">
-        <table class="invoice-list-table datatables-basic table dt-table dt-responsive display table-striped table-sm"
-            style="width:100%">
+        <table class="invoice-list-table datatables-basic table dt-table dt-responsive display table-striped table-sm" style="width:100%">
             <thead>
                 <tr>
                     <th></th>
@@ -41,18 +40,15 @@ nota_admin_render($mysqli,$base_url);
                 Apakah kamu yakin untuk mencetak <b id="modal_cetak_resi_no_invoice"></b>?
             </div>
             <div class="modal-footer">
-                <a target=_blank id="modal_cetak_resi_resi" onclick="$('#modal_cetak_resi').modal('hide')"
-                    class="btn btn-primary">Cetak Resi</a>
-                <a target=_blank id="modal_cetak_resi_invoice" onclick="$('#modal_cetak_resi').modal('hide')"
-                    class="btn btn-primary">Cetak Invoice</a>
+                <a target=_blank id="modal_cetak_resi_resi" onclick="$('#modal_cetak_resi').modal('hide')" class="btn btn-primary">Cetak Resi</a>
+                <a target=_blank id="modal_cetak_resi_invoice" onclick="$('#modal_cetak_resi').modal('hide')" class="btn btn-primary">Cetak Invoice</a>
                 <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
             </div>
         </div>
     </div>
 </div>
 
-<div class="modal fade" id="modal_selesai_produksi" tabindex="-1" aria-labelledby="modal_selesai_produksi_label"
-    aria-hidden="true">
+<div class="modal fade" id="modal_selesai_produksi" tabindex="-1" aria-labelledby="modal_selesai_produksi_label" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -207,10 +203,10 @@ nota_admin_render($mysqli,$base_url);
             "no_invoice": no_invoice
         };
         await $.ajax({
-            url: "<?php echo $base_url;?>/admin/data/add_payment.php",
+            url: "<?php echo $base_url; ?>/admin/data/add_payment.php",
             data: data,
             method: "post",
-            success: function (resultX) {
+            success: function(resultX) {
                 if (resultX == "success") {
                     $('#pelunasan_list_' + no_invoice).remove();
                     delete pelunasan_object[no_invoice];
@@ -226,9 +222,9 @@ nota_admin_render($mysqli,$base_url);
 
     function cetakResi() {
         $("#modal_cetak_resi_no_invoice").html(Object.keys(selected_invoice).join(", "))
-        $("#modal_cetak_resi_resi").attr("href", "<?php echo $base_url?>admin/cetak_resi.php?no_invoice=" +
+        $("#modal_cetak_resi_resi").attr("href", "<?php echo $base_url ?>admin/cetak_resi.php?no_invoice=" +
             Object.keys(selected_invoice).join(","))
-        $("#modal_cetak_resi_invoice").attr("href", "<?php echo $base_url?>admin/cetak_invoice.php?no_invoice=" +
+        $("#modal_cetak_resi_invoice").attr("href", "<?php echo $base_url ?>admin/cetak_invoice.php?no_invoice=" +
             Object.keys(selected_invoice).join(","))
         $("#modal_cetak_resi").modal("show");
     }
@@ -245,10 +241,10 @@ nota_admin_render($mysqli,$base_url);
             "no_invoice": no_invoice
         };
         await $.ajax({
-            url: "<?php echo $base_url;?>/admin/data/selesai_produksi.php",
+            url: "<?php echo $base_url; ?>/admin/data/selesai_produksi.php",
             data: data,
             method: "post",
-            success: function (resultX) {
+            success: function(resultX) {
                 if (resultX == "success") {
                     dt_invoice.ajax.reload();
                 } else {
@@ -261,7 +257,7 @@ nota_admin_render($mysqli,$base_url);
     function get_status_invoice() {
         return $("#status_invoice").val();
     }
-    $(function () {
+    $(function() {
         // Variable declaration for table
         var dt_invoice_table = $('.invoice-list-table');
 
@@ -323,7 +319,7 @@ nota_admin_render($mysqli,$base_url);
                 ],
                 ajax: {
                     "url": "<?php echo $base_url; ?>/admin/data/history.php",
-                    "data": function (d) {
+                    "data": function(d) {
                         d.action = "sales_data";
                         d.status = get_status_invoice();
                     },
@@ -371,7 +367,7 @@ nota_admin_render($mysqli,$base_url);
                 buttons: [{
                     text: '<i class="mdi mdi-plus me-md-1"></i><span class="d-md-inline-block d-none"> Transaksi</span>',
                     className: 'btn btn-primary',
-                    action: function (e, dt, button, config) {
+                    action: function(e, dt, button, config) {
                         alert("Belum ada fitur menambahkan invoice")
                     }
                 }],
@@ -379,14 +375,14 @@ nota_admin_render($mysqli,$base_url);
                 responsive: {
                     details: {
                         display: $.fn.dataTable.Responsive.display.modal({
-                            header: function (row) {
+                            header: function(row) {
                                 var data = row.data();
                                 return 'Details of ' + data['full_name'];
                             }
                         }),
                         type: 'column',
-                        renderer: function (api, rowIdx, columns) {
-                            var data = $.map(columns, function (col, i) {
+                        renderer: function(api, rowIdx, columns) {
+                            var data = $.map(columns, function(col, i) {
                                 return col.title !==
                                     '' // ? Do not show row in modal popup if title is blank (for check box)
                                     ?
@@ -410,12 +406,12 @@ nota_admin_render($mysqli,$base_url);
                         }
                     }
                 },
-                initComplete: function () {
+                initComplete: function() {
                     var column = this;
                     var select = $(
                             '<select id="status_invoice" class="form-select"><option value=""> Cari Status </option></select>'
                         )
-                        .appendTo('.invoice_status').on('change', function () {
+                        .appendTo('.invoice_status').on('change', function() {
                             dt_invoice.ajax.reload();
                         });
                     var option = [{
@@ -457,16 +453,16 @@ nota_admin_render($mysqli,$base_url);
             });
         }
         // On each datatable draw, initialize tooltip
-        dt_invoice_table.on('draw.dt', function () {
+        dt_invoice_table.on('draw.dt', function() {
 
             var tooltipTriggerList = [].slice.call(document.querySelectorAll(
                 '[data-bs-toggle="tooltip"]'));
-            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
                 return new bootstrap.Tooltip(tooltipTriggerEl, {
                     boundary: document.body
                 });
             });
-            $(".checkbox_invoice").each(function (index) {
+            $(".checkbox_invoice").each(function(index) {
                 var no_invoice = $(this).val();
                 if (selected_invoice.hasOwnProperty(no_invoice)) {
                     $(this).prop('checked', true);
